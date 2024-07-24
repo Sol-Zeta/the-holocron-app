7f7a8d4b377b61ba7192c95499d2ad91e6f9b677
Sol-Zeta <mspattoglio@gmail.com>
2024-07-24 21:07:31 +0200

    Implement page and characters states to components

--- src/http/services/characters.ts
+++ src/http/services/characters.ts
@@ -11,7 +11,7 @@ const addIdToCharacter = (charactersData: SwapiCharactersResponse) => {
 };
 
 export const getCharactersByPage = async (
-  page = 1
+  page = '1'
 ): Promise<SwapiCharactersResponse | undefined> => {
   const SWAPI_URL = `https://swapi.trileuco.com/api/people/?page=${page}`;
   try {
