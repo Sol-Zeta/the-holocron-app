5f11a6644737a6cecefebca4ce5b4358b0239d51
Sol-Zeta <mspattoglio@gmail.com>
2024-08-07 19:05:47 +0200

    Create SearchInput

---
+++ src/components/SearchInput/SearchInput.stories.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable */
+import SearchInput from '.';
+
+export default {
+  title: "SearchInput",
+};
+
+export const Default = () => <SearchInput />;
+
+Default.story = {
+  name: 'default',
+};
